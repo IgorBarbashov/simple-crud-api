2e49bb904b8e7d2803e00d6a6bc4081e5f7ad76e
Igor Barbashov <bis200705@gmail.com>
2021-11-28 21:35:13 +0300

    feat: add validation for required fields

--- src/validation/index.js
+++ src/validation/index.js
@@ -2,6 +2,15 @@ const { validate } = require('uuid');
 
 const isIdInUuidFormat = (id) => validate(String(id));
 
+const isAllRequiredFieldsExists = (entity, model) => {
+    const requiredFields = Object.entries(model)
+        .filter(([, value]) => value.required)
+        .map(el => el[0]);
+    const entityFields = Object.keys(entity);
+    return requiredFields.every(el => entityFields.includes(el));
+};
+
 module.exports = {
-    isIdInUuidFormat
+    isIdInUuidFormat,
+    isAllRequiredFieldsExists
 };
